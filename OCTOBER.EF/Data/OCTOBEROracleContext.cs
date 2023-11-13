2777696760e95a4487c3459b04a5d6c6b13f5b9e
Christian Rullan <laptopchris01@gmail.com>
2023-11-13 17:13:28 -0500

    scaffolded data from my schema to October Project

--- OCTOBER.EF/Data/OCTOBEROracleContext.cs
+++ OCTOBER.EF/Data/OCTOBEROracleContext.cs
@@ -17,13 +17,22 @@ namespace OCTOBER.EF.Data
         {
         }
 
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; } = null!;
+        public virtual DbSet<AspNetRoleClaim> AspNetRoleClaims { get; set; } = null!;
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; } = null!;
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; } = null!;
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; } = null!;
+        public virtual DbSet<AspNetUserToken> AspNetUserTokens { get; set; } = null!;
         public virtual DbSet<Course> Courses { get; set; } = null!;
+        public virtual DbSet<DeviceCode> DeviceCodes { get; set; } = null!;
         public virtual DbSet<Enrollment> Enrollments { get; set; } = null!;
         public virtual DbSet<Grade> Grades { get; set; } = null!;
         public virtual DbSet<GradeConversion> GradeConversions { get; set; } = null!;
         public virtual DbSet<GradeType> GradeTypes { get; set; } = null!;
         public virtual DbSet<GradeTypeWeight> GradeTypeWeights { get; set; } = null!;
         public virtual DbSet<Instructor> Instructors { get; set; } = null!;
+        public virtual DbSet<Key> Keys { get; set; } = null!;
+        public virtual DbSet<PersistedGrant> PersistedGrants { get; set; } = null!;
         public virtual DbSet<School> Schools { get; set; } = null!;
         public virtual DbSet<Section> Sections { get; set; } = null!;
         public virtual DbSet<Student> Students { get; set; } = null!;
@@ -33,6 +42,38 @@ namespace OCTOBER.EF.Data
         {
             modelBuilder.HasDefaultSchema("UD_RULLAN")
                 .UseCollation("USING_NLS_COMP");
+
+            modelBuilder.Entity<AspNetUser>(entity =>
+            {
+                entity.HasMany(d => d.Roles)
+                    .WithMany(p => p.Users)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "AspNetUserRole",
+                        l => l.HasOne<AspNetRole>().WithMany().HasForeignKey("RoleId"),
+                        r => r.HasOne<AspNetUser>().WithMany().HasForeignKey("UserId"),
+                        j =>
+                        {
+                            j.HasKey("UserId", "RoleId");
+
+                            j.ToTable("ASP_NET_USER_ROLES");
+
+                            j.HasIndex(new[] { "RoleId" }, "IX_ASP_NET_USER_ROLES_ROLE_ID");
+
+                            j.IndexerProperty<string>("UserId").HasColumnName("USER_ID");
+
+                            j.IndexerProperty<string>("RoleId").HasColumnName("ROLE_ID");
+                        });
+            });
+
+            modelBuilder.Entity<AspNetUserLogin>(entity =>
+            {
+                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+            });
+
+            modelBuilder.Entity<AspNetUserToken>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
+            });
 
             modelBuilder.Entity<Course>(entity =>
             {
