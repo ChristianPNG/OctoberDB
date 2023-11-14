a6984a4896f2fa5bca76189b0725877b3c4c8b7b
Christian Rullan <laptopchris01@gmail.com>
2023-11-13 23:52:28 -0500

    added GradeTypeWeight controller

---
+++ Server/Controllers/UD/GradeTypeWeightController.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using OCTOBER.EF.Data;
+using OCTOBER.EF.Models;
+using OCTOBER.Server.Controllers.Base;
+using OCTOBER.Shared.DTO;
+
+namespace OCTOBER.Server.Controllers.UD
+{
+    public class GradeTypeWeightController : BaseController
+    {
+        public GradeTypeWeightController(OCTOBEROracleContext context,
+            IHttpContextAccessor httpContextAccessor,
+            IMemoryCache memoryCache)
+        : base(context, httpContextAccessor)
+        {
+        }
+
+        [HttpDelete]
+        [Route("Delete/{SchoolID}/{SectionID}/{GradeTypeCode}")]
+        public async Task<IActionResult> Delete(int SchoolID, int SectionID, string GradeTypeCode)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeTypeWeights.Where(x => x.SchoolId == SchoolID).Where(x => x.SectionId == SectionID).Where(x => x.GradeTypeCode == GradeTypeCode).FirstOrDefaultAsync();
+
+                if (itm != null)
+                {
+                    _context.GradeTypeWeights.Remove(itm);
+                }
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var result = await _context.GradeTypeWeights.Select(sp => new GradeTypeWeightDTO
+                {
+                    NumberPerSection = sp.NumberPerSection,
+                    PercentOfFinalGrade = sp.PercentOfFinalGrade,
+                    DropLowest = sp.DropLowest,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate,
+                })
+                .ToListAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpGet]
+        [Route("Get/{SchoolID}/{SectionID}/{GradeTypeCode}")]
+        public async Task<IActionResult> Get(int SchoolID, int SectionID, string GradeTypeCode)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                GradeTypeWeightDTO? result = await _context
+                    .GradeTypeWeights
+                    .Where(x => x.SectionId == SectionID)
+                    .Where(x => x.SchoolId == SchoolID)
+                    .Where(x => x.GradeTypeCode == GradeTypeCode)
+                     .Select(sp => new GradeTypeWeightDTO
+                     {
+                         NumberPerSection = sp.NumberPerSection,
+                         PercentOfFinalGrade = sp.PercentOfFinalGrade,
+                         DropLowest = sp.DropLowest,
+                         CreatedBy = sp.CreatedBy,
+                         CreatedDate = sp.CreatedDate,
+                         ModifiedBy = sp.ModifiedBy,
+                         ModifiedDate = sp.ModifiedDate,
+                     })
+                .SingleOrDefaultAsync();
+
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpPost]
+        [Route("Post")]
+        public async Task<IActionResult> Post([FromBody] GradeTypeWeightDTO _GradeTypeWeightDTO)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeTypeWeights
+                    .Where(x => x.SchoolId == _GradeTypeWeightDTO.SchoolId)
+                    .Where(x => x.SectionId == _GradeTypeWeightDTO.SectionId)
+                    .Where(x => x.GradeTypeCode == _GradeTypeWeightDTO.GradeTypeCode)
+                    .FirstOrDefaultAsync();
+
+                if (itm == null)
+                {
+                    GradeTypeWeight c = new GradeTypeWeight
+                    {
+                        NumberPerSection = _GradeTypeWeightDTO.NumberPerSection,
+                        PercentOfFinalGrade = _GradeTypeWeightDTO.PercentOfFinalGrade,
+                        DropLowest = _GradeTypeWeightDTO.DropLowest,
+                    };
+                    _context.GradeTypeWeights.Add(c);
+                    await _context.SaveChangesAsync();
+                    await _context.Database.CommitTransactionAsync();
+                }
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpPut]
+        [Route("Put")]
+        public async Task<IActionResult> Put([FromBody] GradeTypeWeightDTO _GradeTypeWeightDTO)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeTypeWeights
+                    .Where(x => x.SchoolId == _GradeTypeWeightDTO.SchoolId)
+                    .Where(x => x.SectionId == _GradeTypeWeightDTO.SectionId)
+                    .Where(x => x.GradeTypeCode == _GradeTypeWeightDTO.GradeTypeCode).FirstOrDefaultAsync();
+
+                itm.NumberPerSection = _GradeTypeWeightDTO.NumberPerSection;
+                itm.PercentOfFinalGrade = _GradeTypeWeightDTO.PercentOfFinalGrade;
+                itm.DropLowest = _GradeTypeWeightDTO.DropLowest;
+
+                _context.GradeTypeWeights.Update(itm);
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+    }
+}
